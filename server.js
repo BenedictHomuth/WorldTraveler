6f1fb7f4ef5513b2085e7eea5ffd11f079f1124d
BenedictHomuth <benedict.homuth@gmail.com>
2020-02-03 21:05:03 +0100

    Next Commit

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const app = express();
 app.listen(3000, () => console.log("listening on port 3001"));
 
 app.use(express.static("public"));
-app.use(express.static("stuff"));//Test
+app.use(express.static("stuff"));//Test333
