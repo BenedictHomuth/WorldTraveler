75b27cfda96ed6c083e312ca2b23e2efe8f345a8
BenedictHomuth <benedict.homuth@gmail.com>
2020-02-03 21:04:01 +0100

    inital commit

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+
+app.listen(3000, () => console.log("listening on port 3001"));
+
+app.use(express.static("public"));
+app.use(express.static("stuff"));//Test
