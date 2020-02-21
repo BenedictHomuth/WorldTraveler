611e75a2bf9a98a82f06ce61ecf2a56e3b1a4734
BenedictHomuth <benedict.homuth@gmail.com>
2020-02-21 12:46:33 +0100

    added the modal to the homepage!
    Need to clean up scripts later on!
    
    Issue when delete old scripts with map tiles beeing loaded :/

--- public/home/modal.js
+++ public/home/modal.js
@@ -1,2 +1,47 @@
-var btn = document.getElementById("");
-$('#myModal').modal("show");
+var openModalBtn = document.getElementById("myBtn");
+
+
+openModalBtn.addEventListener("click", function(){
+ $("#myModal").modal("show");
+ var saveTripBtn = document.getElementById("saveChangesBtn");
+ var dicardTripBtn = document.getElementById("discardChangesBtn");
+})
+
+saveTripBtn.addEventListener("click", ()=>{
+    var cityName = document.getElementById("cityName").value;
+    var date = document.getElementById("datePicked").value;
+    var comment = document.getElementById("commentVisit").value;
+    console.log(cityName);
+    console.log(date);
+    console.log(comment);
+})
+
+dicardTripBtn.addEventListener("click", ()=>{
+    console.log("Your trip was not saved!");
+})
+
+/*
+$(window).load(function () {
+    var openModalBtn = document.getElementById("myBtn");
+
+
+    openModalBtn.addEventListener("click", function () {
+        $("#myModal").modal("show");
+        var saveTripBtn = document.getElementById("saveChangesBtn");
+        var dicardTripBtn = document.getElementById("discardChangesBtn");
+    })
+
+    saveTripBtn.addEventListener("click", () => {
+        var cityName = document.getElementById("cityName").value;
+        var date = document.getElementById("datePicked").value;
+        var comment = document.getElementById("commentVisit").value;
+        console.log(cityName);
+        console.log(date);
+        console.log(comment);
+    })
+
+    dicardTripBtn.addEventListener("click", () => {
+        console.log("Your trip was not saved!");
+    })
+});
+*/
