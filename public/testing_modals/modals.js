27509f48e76c2f09dc1c465f16474de167ff028b
BenedictHomuth <benedict.homuth@gmail.com>
2020-02-21 12:14:02 +0100

    Added JS to modal, so I can read data from it!

---
+++ public/testing_modals/modals.js
@@ -0,0 +1,21 @@
+var openModalBtn = document.getElementById("myBtn");
+
+
+openModalBtn.addEventListener("click", function(){
+ $("#myModal").modal("show");
+ var saveTripBtn = document.getElementById("saveChangesBtn");
+ var dicardTripBtn = document.getElementById("discardChangesBtn");
+})
+
+saveTripBtn.addEventListener("click", ()=>{
+    var cityName = document.getElementById("cityName").value;
+    var date = document.getElementById("datePicked").value;
+    var comment = document.getElementById("commentVisit").value;
+    console.log(cityName);
+    console.log(date);
+    console.log(comment);
+})
+
+dicardTripBtn.addEventListener("click", ()=>{
+    console.log("Your trip was not saved!");
+})
